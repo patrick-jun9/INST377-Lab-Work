c56ff6229acad50401d636ff51163b712ed41ba0
patrick-jun9 <98505923+patrick-jun9@users.noreply.github.com>
2022-02-20 21:56:19 -0500

    script

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -2,4 +2,43 @@ let slidePosition = 0;
 const slides = document.querySelectorAll('carousel_item');
 const totalSlides = slides.length;
 
-console.log(totalSlides)
+console.log(slides);
+
+document.
+    querySelectorAll('carousel_button--next');
+    .addEventListener("click", function(){
+        moveToNextSlide();
+    });
+
+document.
+    querySelectorAll('carousel_button--next');
+    .addEventListener("click", function(){
+        moveToPrevSlide();
+    });
+
+function updateSlidePosition(){
+    for(let slide of slides){
+        slide.classList.remove('carousel_item_visible')
+        slide.classList.add('carousel_item_hidden')
+    }
+    slides[slidesPosition].classList.add('carousel_item_visible');
+}    
+
+function moveToNextSlide(){
+    
+    if(slidePosition == totalSlides-1 ){
+    slidePosition = 0;
+}  else {
+    slidePosition++;
+}
+updateSlidePosition();
+}
+
+function moveToPrevSlide(){
+if(slidePosition==0){
+    slidePosition = totalSlides-1;  
+} else {
+    slidePosition--;
+}
+updateSlidePosition()
+}
