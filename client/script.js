9a953ef39f0b9708a0cdd0a52e7e061c01e263f2
patrick-jun9 <98505923+patrick-jun9@users.noreply.github.com>
2022-04-11 15:41:53 -0400

    script

--- client/script.js
+++ client/script.js
@@ -42,7 +42,7 @@ function initMap(targetId) {
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
+    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
   }).addTo(map);
   return map;
 }
